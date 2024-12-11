d775632ac7cdf11954b5b3c697664acc4b9ee442
froozy3 <tsemkalo2006@gmail.com>
2024-12-11 20:31:49 +0200

    Add .gitignore

--- EventManagment/settings.py
+++ EventManagment/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,4 +134,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+}
+# then delete this for security 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    #
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
--- events/models.py
+++ events/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Event(models.Model):
+
     title = models.CharField(max_length=255)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     location = models.CharField(max_length=255)
-    organaizer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    organizer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
